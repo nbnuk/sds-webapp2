cbb0d2120b1ac310fdd9e53b039e03840cb3494f
Doug Palmer <Doug.Palmer@csiro.au>
2019-05-06 11:10:05 +1000

    Pre 1.4.5 release Update to JDK 1.8

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,8 +6,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
-grails.project.target.level = 1.7
-grails.project.source.level = 1.7
+grails.project.target.level = 1.8
+grails.project.source.level = 1.8
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolver = "maven" // or ivy
@@ -32,7 +32,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.27'
         // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
-        compile("au.org.ala:sds:1.3.0") {
+        compile("au.org.ala:sds:1.4.5") {
             excludes "spring-jdbc"
 	        excludes "servlet-api"
         }
@@ -42,17 +42,17 @@ grails.project.dependency.resolution = {
         build ":release:3.0.1"
         //provided ":grails-plugin-async:2.3.7"
         // plugins for the build system only
-        build ":tomcat:7.0.52.1"
+        build ":tomcat:7.0.54"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.2"
+        compile ":scaffolding:2.1.0"
        // compile ':cache:1.1.8'
         compile ":quartz:1.0.1"
 
 
         // plugins needed at runtime but not for compilation
-        runtime ":database-migration:1.3.8"
-        runtime ":ala-bootstrap2:2.1"
+        runtime ":database-migration:1.4.0"
+        runtime ":ala-bootstrap2:2.7.0"
         runtime ":resources:1.2.14"
         if (Environment.current == Environment.PRODUCTION) {
             runtime ":zipped-resources:1.0.1"
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
             compile ":cache-headers:1.1.7"
             runtime ":yui-minify-resources:0.1.5"
         }
-        runtime ':ala-auth:1.2'
+        runtime ':ala-auth:2.2.0'
         
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
